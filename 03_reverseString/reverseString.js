eb5dec0049ee87d39ed5e44c8bd3fdfb48598723
Amrit Clair <amrit_clair@hotmail.com>
2022-03-21 22:39:17 -0400

    reverseString JS exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let reversalArray = [];  
+let reversalNum = word.length-1; 
 
+for (let i=0; i<word.length; i++){
+    reversalArray[i] = word[reversalNum]; 
+    reversalNum -= 1;  
 };
 
+
+return reversalString = reversalArray.join('');
+}
 // Do not edit below this line
 module.exports = reverseString;
