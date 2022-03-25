bd169000f9d6543af656f656f9db4a977f58fb2c
Amrit Clair <amrit_clair@hotmail.com>
2022-03-24 23:59:13 -0400

    sumAll JS exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    if ((typeof(a) !== 'number') || (typeof(b) !== 'number') || (a<0) || (b<0)) {
+        return 'ERROR';
+    }
+
+    if (a === b){
+        return a;
+    }
+    
+    (a>b)? iteration = a-b : iteration = b-a; 
+    (a>b)? initalSum = b: initalSum = a;
+
+    let continuousSum = initalSum; 
+
+    for (let i=1; i<=iteration; i++){
+        continuousSum = continuousSum + (initalSum+i)
+    }
+
+    return continuousSum;
+
 
 };
 
