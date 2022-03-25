49399f37bce2bafc815030a17861d3c93e203321
Amrit Clair <amrit_clair@hotmail.com>
2022-03-24 23:59:53 -0400

    removeFromArray exercise - need to review fn(args)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    //const parameterArray = Array.from(arguments);
+    const inputArray = args[0]; 
+    //const removeArray = parameterArray.slice(1);
+    const filteredArray = [];  
+
+
+    for (let i=0; i<inputArray.length; i++){
+        if (!(args.includes(inputArray[i]))){
+            filteredArray.push(inputArray[i]);
+        }
+
+    }
+    console.log(filteredArray);
+return filteredArray;
 };
 
 // Do not edit below this line
