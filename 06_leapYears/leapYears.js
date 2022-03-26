ba86ef8a60ce23af64f178d09efa60a2da495cfe
Amrit Clair <amrit_clair@hotmail.com>
2022-03-25 23:40:19 -0400

    JS exercises.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if ((year<=400) && (year%4==0)){ //years less than (and including) 400. 
+        return true;
+    }
+
+    if (((year >= 400) && (year%400 === 0)) && ((((year%400==0)&&(year%100==0)))||(year%4==0))){ // years greater than 400, for centuries divisible by 400.
+        return true;
+    }
+
+    if ((year >= 400)&&(year%4==0)&&(year%100 != 0)){ //for years greater than 400, non-centuries. 
+       return true; 
+    }
+
+    return false; 
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+
+  const degrees = (temp-32)*5/9;
+
+  const roundedDegrees = Math.round(degrees * 10)/10; 
+  
+  return roundedDegrees;
 
 };
 
-const ctof = function() {
+const ctof = function(degrees) {
 
+  const temp = (degrees*9/5) + 32; 
+
+  const roundedTemp = Math.round(temp *10)/10; 
+
+  return roundedTemp;
 };
 
 // Do not edit below this line
