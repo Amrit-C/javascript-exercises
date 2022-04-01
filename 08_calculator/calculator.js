2c5f885ba25f32e473d95db47c1ba56e6a5750d1
Amrit Clair <amrit_clair@hotmail.com>
2022-04-01 18:59:33 -0400

    JS Exercises.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b; 
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(array) {
+	const sumOfArray = array.reduce((firstElement, nextElement) =>
+  firstElement + nextElement, 0);
+
+return sumOfArray;
+}
+
+const multiply = function(array) {
+  const multipliedArray = array.reduce((firstElement, nextElement) =>
+  firstElement * nextElement, 1);
+  
+  return multipliedArray;
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(a) {
 
-const sum = function() {
-	
-};
+if (a==0){
+  return 1;
+}
 
-const multiply = function() {
+let result = a;  
 
-};
+while (a > 1){
+  a--;
+  result *=  a;
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+return result;
 };
 
 // Do not edit below this line
