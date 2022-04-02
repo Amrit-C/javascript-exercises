4e789371a12532cae8441159bcc2b6b5a4d22408
Amrit Clair <amrit_clair@hotmail.com>
2022-04-01 20:32:31 -0400

    JS exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+
+    if (member <=0){
+        return "OOPS";
+    }
+
+    fibon = [1, 1]; 
+
+    for (let i=2; i<=member-1; i++){
+        fibon[i] = (fibon[i-1]+fibon[i-2]);
+    }
+
+    return fibon[member-1];
 
 };
 
